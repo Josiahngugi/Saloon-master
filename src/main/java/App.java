137a42f28db48e471d8ba751f00fef6122326aab
josiah <mainajosiahdev@gmail.com>
2018-10-15 08:22:36 +0300

    collecting data from Stylist users

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 
 public class App {
@@ -15,6 +16,26 @@ public class App {
             model.put("stylists",null);
             model.put("stylists",db.all());
             model.put("template","templates/stylist.vtl");
+            return new ModelAndView(model,"templates/layout.vtl");
+        },new VelocityTemplateEngine());
+
+        post("/addStylist",(req,res)->{
+            String id=req.queryParams("id");
+            Stylist stylist=new Stylist(
+                    Double.parseDouble(id),
+                    req.queryParams("fname"),
+                    req.queryParams("lname"),
+                    req.queryParams("phone"),
+                    req.queryParams("email")
+            );
+            System.out.println(stylist.getPhone());
+            if(stylist.save()){
+                model.put("message","Successfully Added ");
+            }
+            else{
+                model.put("message","Stylist Exists");
+            }
+            res.redirect("/");
             return new ModelAndView(model,"templates/layout.vtl");
         },new VelocityTemplateEngine());
     }
