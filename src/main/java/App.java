6a7160380a3c136779e003cf5d5ecc8e9674f540
josiah <mainajosiahdev@gmail.com>
2018-10-15 08:12:20 +0300

    Sql2o and Spark imports

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,22 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+
 public class App {
+    static DB db=new DB();
+    static Map<String,Object> model=new HashMap<String,Object>();
+    public static void main(String[]args){
+        get("/",(req,res)->{
+            model.put("stylists",null);
+            model.put("stylists",db.allData());
+            model.put("template","templates/stylist.vtl");
+            return new ModelAndView(model,"templates/layout.vtl");
+        },new VelocityTemplateEngine());
+    }
 }
